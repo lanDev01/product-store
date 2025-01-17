b1bbd088f6f574827bede5cfac41aa25dab4b178
LanDev01 <alnjr0001@outlook.com>
2025-01-17 09:09:40 -0300

    Feat: add SnackBar

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -4,7 +4,8 @@ import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductsService } from '../../shared/services/products.service';
-
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -14,7 +15,9 @@ import { ProductsService } from '../../shared/services/products.service';
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
-  productService = inject(ProductsService)
+  productService = inject(ProductsService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
 
   form = new FormGroup({
     title: new FormControl<string>('', {
@@ -24,10 +27,20 @@ export class CreateComponent {
   });
 
   onSubmit() {
+    const title = this.form.controls.title.value
+
+    if (title === '') return
+
     this.productService.post({
-      title: this.form.controls.title.value
+      title,
     }).subscribe(() => {
-      alert('Sucesso!');
+      this.matSnackBar.open('Produto criado com sucesso!', 'Ok', {
+        duration: 3000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top'
+      });
+
+      this.router.navigateByUrl('/')
     })
   }
 }
