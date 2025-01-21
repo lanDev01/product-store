379583fa15f984b95731ffd3c63f6da335551f2c
LanDev01 <alnjr0001@outlook.com>
2025-01-21 13:40:36 -0300

    refactor: create smart functions resolvers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
-import { ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
-import { inject } from '@angular/core';
-import { ProductsService } from './shared/services/products.service';
+import { getProducts } from './shared/resolvers/get-products.resolver';
+import { getProduct } from './shared/resolvers/get-product.resolver';
 
 export const routes: Routes = [{
   path: "",
   resolve: {
-    products: () => {
-      const productService = inject(ProductsService);
-      return productService.getAllProducts();
-    }
+    products: getProducts
   },
   component: ListComponent
 },
@@ -20,11 +17,7 @@ export const routes: Routes = [{
 {
   path: 'edit-product/:id',
   resolve: {
-    product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-      const productService = inject(ProductsService);
-
-      return productService.get(route.paramMap.get('id') as string);
-    }
+    product: getProduct
   },
   loadComponent: () => import('./features/edit/edit.component').then(m => m.EditComponent)
 }
