abebb05555a0eaecda56669087114a63b8c5dfa9
LanDev01 <alnjr0001@outlook.com>
2025-11-07 13:46:58 -0300

    refactor: clean up app.component.ts and app.config.ts for improved readability and organization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-export class AppComponent { }
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
 import type { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app.routes';
+
+import Lara from '@primeuix/themes/lara';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimationsAsync(),
-    provideHttpClient()
-  ]
+    provideHttpClient(),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+      },
+    }),
+  ],
 };
